f11233b2858068f68af3e01cc5d57e0ed20202f2
Rich Hood <RiHood@jhacorp.com>
2023-07-25 19:00:35 -0400

    amost through errors

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -7,21 +7,23 @@
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
-fn main() -> Result<i32, ParseIntError>{
+fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
-
-    let cost = total_cost(pretend_user_input)?;
-
-    if cost > tokens {
-        println!("You can't afford that many!");
-    } else {
-        tokens -= cost;
-        println!("You now have {} tokens.", tokens);
+    match total_cost(pretend_user_input) {
+        Ok(cost) => { 
+            if cost > tokens {
+                println!("You can't afford that many!");
+            } else {
+                tokens -= cost;
+                println!("You now have {} tokens.", tokens);
+            }
+        },
+        Err(e) => {
+           println!("{}",e.to_string());
+        },
     }
 }
 
